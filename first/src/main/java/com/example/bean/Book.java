1d31f7a05f8afd7a7a2cbc6dc4ea9e74287e1b21
aman <123>
2017-11-08 10:48:35 +0800

    SecondController

---
+++ first/src/main/java/com/example/bean/Book.java
@@ -0,0 +1,16 @@
+package com.example.bean;
+
+/**
+ * Created by Administrator on 2017.11.07 0007.
+ */
+public class Book {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
